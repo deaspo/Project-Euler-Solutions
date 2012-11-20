ebad47b2fe6a9841b766c26623310fb2a9b2153e
Magnus Olsson <magnus.olsson@projectplace.com>
2012-11-20 20:57:34 +0100

    Made solution 92 faster

--- Problem 92/solution92.py
+++ Problem 92/solution92.py
@@ -1,17 +1,26 @@
 def main():
 	limit = 10000000
 	answer = 0
-	for starting_number in range(1, limit):
+	numbers = range(1, limit)
+	for starting_number in numbers:
 		number = starting_number
-		number_series = [number]
+		visited_numbers = [number]
 		while number not in (1, 89):
-			number = get_next_number(number)
-		if number == 89:
-			answer += 1
+			number = get_next_number(number, numbers)
+			visited_numbers.append(number)
+		for visited_number in visited_numbers:
+			numbers[visited_number - 1] = number
+		answer += number == 89
+
 	return answer
 
-def get_next_number(number):
-	return sum([int(digit)*int(digit) for digit in str(number)])
+def get_next_number(number, numbers):
+	squared_digit_sum = 0
+	while number > 0:
+		squared_digit_sum += (number % 10)**2
+		number /= 10
+
+	return numbers[squared_digit_sum - 1]
 
 if __name__ == "__main__":
     from time import time
