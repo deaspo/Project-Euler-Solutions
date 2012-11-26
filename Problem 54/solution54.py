f663375c3f681894923b9780fb58827e021ca1a9
Magnus Olsson <magnus.olsson@projectplace.com>
2012-11-26 20:07:30 +0100

    Simplified read from file algorithm for Problem 54

--- Problem 54/solution54.py
+++ Problem 54/solution54.py
@@ -1,43 +1,29 @@
 #! /usr/bin/env python 
 # -*- coding: utf-8 -*-
+pip = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+suit = {'H':1, 'S':2, 'D':3, 'C':4}
+def main():
+   player1, player2 = getHands('poker.txt')
 
-from copy import *
 
 def getHands(file):
    """Reads hands from file and deals them out to the players """
    cards = []
    file = open(file, "r")
-
-   for line in file:
-      card = ''
-      for char in line:
-         if char != '\n' and char != ' ':
-            card = card + char
-            if len(card) == 2:
-               cards.append(card)
-               card = ''
+   cards = [char for line in file for char in line if char != '\n' and char != ' ' and char != '\r']
+   cards = [i+j for i,j in zip(cards[::2],cards[1::2])]
    file.close()
 
-   player1 = []
-   player2 = []
-   hand = []
-   i = 0
-   for card in cards:
-      i += 1
-      hand.append(card)      
-      if i == 5:
-         player1.append(hand)
-         hand = []
-      elif i == 10:
-         player2.append(hand)
-         hand = []
-         i = 0
+   player1 = [cards[5*i:5*i+5] for i in xrange(0, len(cards)/5, 2)]
+   player2 = [cards[5*i+5:5*i+10] for i in xrange(0, len(cards)/5, 2)]
+
    return player1, player2
 
 if __name__ == "__main__":
-   value = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, \
-            '10':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
-   suit = {'Heart':1, 'Spade':2, 'Diamond':3, 'Club':4}
-   player1, player2 = getHands('poker.txt')
-   answer = 0
+   from time import time
+   
+   start = time()
+   answer = main()
+   stop = time()
    print 'Answer:', answer
+   print 'Time:', stop - start
